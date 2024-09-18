b1502427614e3290fa00a9537b6cd9d858e4ff3b
Cristina <ccarneiroeducacion@gmail.com>
2024-09-18 10:16:22 +0200

    Cambiar sitios de ping a bing, yahoo

--- script.ps1
+++ script.ps1
@@ -1,4 +1,4 @@
-﻿       $servers = @("10.57.401.196", "yahoo.com", "4.4.4.4")
+﻿       $servers = @("10.57.401.196", "yahoo.com", "bing.com")
        foreach ($server in $servers) {
            Test-Connection -ComputerName $server -Count 2
        }
